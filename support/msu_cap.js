8f491df06e6ce2c2777f751849b152c6ae90f8c8
James Gosnell <jamesgosnell@gmail.com>
2011-10-16 03:03:22 -0500

    ADDED msu_gen.js randomization and comments to msu_cap.js

--- support/msu_cap.js
+++ support/msu_cap.js
@@ -1,15 +1,19 @@
 zmq = require( 'zeromq' )
 
+//create the subscriber socket with keyword 'sub'
 console.log("Connecting...")
 subscriber = zmq.createSocket( 'sub' )
 
+//connect the subscriber to the publisher
 subscriber.connect( "tcp://localhost:5000" )
 
-subscriber.subscribe('Well')
+//subscribe to a certain text that matches like a wildcard
+subscriber.subscribe('')
 subscriber.on( 'message', function (data) {
   console.log( data.toString() )
 })
 
+//gracefully exit the program
 process.on('SIGINT', function() {
   console.log( "\ngracefully shutting down from  SIGINT (Crtl-C)" )
   subscriber.close()
--- support/msu_gen.js
+++ support/msu_gen.js
@@ -5,7 +5,15 @@ publisher = zmq.createSocket( 'pub' )
 console.log( "connecting..." )
 publisher.bindSync( "tcp://*:5000" )
 
-var idInterval = setInterval( function() {publisher.send( "Well hello thar!" )} , 1000)
+function rand( max,min ) {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+function msu(){ return rand( 999,100 ) +
+  "-" + rand( 999,100 ) + "-" + rand( 9999,1000 ) + 
+  " INIT " + rand( 99,0 ) + "." + rand( 99,0 ) + "." + rand( 99,0 ) + 
+  " " + rand( 99999,0 ) }
+var idInterval = setInterval( function() {publisher.send( msu() )} , 1000)
 
 process.on('SIGINT', function() {
   console.log( "\ngracefully shutting down from  SIGINT (Crtl-C)" )
