6ed01959a8294c9346ee2a5dbff288dc6edd9f3b
James Gosnell <jamesgosnell@gmail.com>
2011-10-29 22:46:42 -0500

    ADDED socket.io connection to client

--- public/javascripts/netpeek.js
+++ public/javascripts/netpeek.js
@@ -4,19 +4,25 @@ dojo.require("dojo.store.Memory");
 dojo.require("dojo.store.Observable");
 
 dojo.ready(function(){
+  var socket = io.connect('http://localhost');
+  socket.on( 'msu', function(data) {
+    console.log(data);
+  });
+
     var data = [
         {"name": "Dow Jones", "index": 12197.88, "date": new Date()},
         {"name": "Nasdaq", "index": 2730.68, "date": new Date()},
         {"name": "S&P 500", "index": 1310.19, "date": new Date()}
     ];
-//create the store with the data
 
-marketStore = new dojo.store.Memory({data: data});
-// wrap the store with Observable to make it possible to monitor:
-marketStore = dojo.store.Observable(marketStore);
+  //create the store with the data
 
-function viewResults(results){
-        var container = dojo.byId("container");
+  marketStore = new dojo.store.Memory({data: data});
+  // wrap the store with Observable to make it possible to monitor:
+  marketStore = dojo.store.Observable(marketStore);
+
+  function viewResults(results){
+    var container = dojo.byId("container");
             var rows = [];
     function addRow(market, i){
         rows.splice(i, 0, dojo.create("div", {
