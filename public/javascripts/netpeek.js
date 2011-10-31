806f943bb3f3b15c7ec7d19862528d5c89af1806
James Gosnell <jamesgosnell@gmail.com>
2011-10-30 23:26:31 -0500

    REMOVED some old test code from Dojo Toolkit demo

--- public/javascripts/netpeek.js
+++ public/javascripts/netpeek.js
@@ -1,47 +1,45 @@
-var marketStore;
+var msuStore;
 
 dojo.require("dojo.store.Memory");
 dojo.require("dojo.store.Observable");
 
 dojo.ready(function(){
+
+  var store;
+  // Socket.IO connection
   var socket = io.connect('http://localhost');
   socket.on( 'msu', function(data) {
     console.log(data);
+    store = data;
   });
-
-    var data = [
-        {"name": "Dow Jones", "index": 12197.88, "date": new Date()},
-        {"name": "Nasdaq", "index": 2730.68, "date": new Date()},
-        {"name": "S&P 500", "index": 1310.19, "date": new Date()}
-    ];
 
   //create the store with the data
 
-  marketStore = new dojo.store.Memory({data: data});
+  msuStore = new dojo.store.Memory({data: store});
   // wrap the store with Observable to make it possible to monitor:
-  marketStore = dojo.store.Observable(marketStore);
+  msuStore = dojo.store.Observable(msuStore);
 
   function viewResults(results){
     var container = dojo.byId("container");
             var rows = [];
-    function addRow(market, i){
+    function addRow(msu, i){
         rows.splice(i, 0, dojo.create("div", {
-            innerHTML: market.name + " index: " + market.index.toFixed(2) + " at: " + market.date.toLocaleTimeString() }, container, i));
+            innerHTML: msu.name + " index: " + msu.args }, container, i));
     }
     function removeRow(i){
         dojo.destroy(rows.splice(i, 1)[0]);
     }
     results.forEach(addRow);
-    results.observe(function(market, removedFrom, insertedInto){
+    results.observe(function(msu, removedFrom, insertedInto){
         if(removedFrom > -1){
             removeRow(removedFrom);
         }
         if(insertedInto > -1){
-            addRow(market, insertedInto);
+            addRow(msu, insertedInto);
         }
     }, true);
 }
-var results = marketStore.query({});
+var results = msuStore.query({});
 viewResults(results);
 
 });
